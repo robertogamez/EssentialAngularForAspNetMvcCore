3067ffa4a673bc5b3c5d49271b1cd13e9128b07e
robertogamez <vn00m8demail.wal/mart.com>
2019-01-03 10:00:09 -0600

    Configurando aplicacion con bootstrap y angular

--- SportsStoreSPA/Program.cs
+++ SportsStoreSPA/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -19,6 +20,12 @@ namespace SportsStoreSPA
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging(ConfigLogging)
                 .UseStartup<Startup>();
+
+        static void ConfigLogging(ILoggingBuilder bldr)
+        {
+            bldr.AddFilter(DbLoggerCategory.Database.Connection.Name, LogLevel.Information);
+        }
     }
 }
